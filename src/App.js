518274ed7cecdc04c396b504ed02e28497a1c66c
Afaq Ahmad <afaqaijaz66@gmail.com>
2020-06-26 09:09:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-      <h1>Lesson Three</h1>
-
-    </div>
+    <h1>Hello World This is lesson Three</h1>
   );
 }
 
