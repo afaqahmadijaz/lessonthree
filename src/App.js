37fa10d0e8d9dcfe3a59c6d8186066ec3bd9107a
Afaq Ahmad <afaqaijaz66@gmail.com>
2020-06-26 10:35:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './App.css';
+import Dinner from './Dinner.js';
 
 function App() {
   return (
-    <h1>Hello World This is lesson Three</h1>
+    <div className = "App">
+      <h1>Hello World This is lesson Three</h1>
+      <Dinner DinshName = "Chicken karhai" SweetDish = "Gajr ka halwa"/>
+    </div>
   );
 }
 
